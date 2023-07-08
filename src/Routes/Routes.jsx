934e378fd8ef9e7843353c0d5bb8f35f015783e2
nahidthenh <nhgroup18@gmail.com>
2023-07-08 08:29:08 +0600

    Header

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Error from '../pages/Error'
 import Main from "../layout/Main";
 import Home from "../pages/Home";
+import Blog from "../pages/Blog";
 export const router = createBrowserRouter([
     {
         path: "/",
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
                 path: '/',
                 element: <Home></Home>
             },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            }
         ]
     },
     {
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,27 +1,29 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 import headerLogo from '../assets/images/logo.png'
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="">
-                    <img className="header-logo" src={headerLogo} alt="" />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto"></Nav>
-                    <Nav>
-                        <Nav.Link as={Link} to='/home'>Home</Nav.Link>
-                        <Nav.Link as={Link} to='/destination'>Destination</Nav.Link>
-                        <Nav.Link as={Link} to='/blog'>Blog</Nav.Link>
-                        <Nav.Link as={Link} to='/contact'>Contact</Nav.Link>
-                        <Nav.Link as={Link} to='/profile' >Md. Nahid Hasan</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <>
+            <Navbar collapseOnSelect expand="lg" className="bg-white py-3">
+                <Container>
+                    <Navbar.Brand as={Link} to='/'>
+                        <img className="header-logo" src={headerLogo} alt="" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto"></Nav>
+                        <Nav>
+                            <NavLink as={Link} to='/'>Home</NavLink>
+                            <NavLink as={Link} to='/destination'>Destination</NavLink>
+                            <NavLink as={Link} to='/blog'>Blog</NavLink>
+                            <NavLink as={Link} to='/contact'>Contact</NavLink>
+                            <NavLink as={Link} to='/profile' >Md. Nahid Hasan</NavLink>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     );
 };
 
