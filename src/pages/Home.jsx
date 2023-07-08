abba0490b05dcfecfe6b72a8dcf21cec3a0e58af
nahidthenh <nhgroup18@gmail.com>
2023-07-08 10:33:21 +0600

    Home: hero area

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,38 @@ const Home = () => {
     return (
         <Container className="default-margin-top">
             <Row>
-                <Col>
-                    
+                <Col lg={6} sm={12} md={12}>
+                    <div className="hero-content">
+                        <h1>Where would you like to go?</h1>
+                        <p>Embark on your next extraordinary adventure.</p>
+                        <div className="form-inner">
+                            <form className="booking-form">
+                                <div className="row">
+                                    <div className="col-lg-6 col-md-6 col-sm-12 column">
+                                        <div className="form-group">
+                                            <input type="text" name="origin" placeholder="Origin" required="" />
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-6 col-md-6 col-sm-12 column">
+                                        <div className="form-group">
+                                            <input type="text" name="destination" placeholder="Destination" required="" />
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-6 col-md-6 col-sm-12 column">
+                                        <div className="form-group input-date">
+                                            <i className="far fa-angle-down"></i>
+                                            <input type="date" name="date" placeholder="When?" className="when" />
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-6 col-md-6 col-sm-12 column">
+                                        <div className="form-group message-btn">
+                                            <button type="submit" className="theme-btn"><i className="far fa-search"></i>Find Now</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </Col>
             </Row>
         </Container>
