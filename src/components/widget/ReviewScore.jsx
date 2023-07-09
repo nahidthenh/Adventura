26fd7381ac0117615b4f32aa46aa91fc79ea01be
nahidthenh <nhgroup18@gmail.com>
2023-07-09 19:03:47 +0600

    Review Score Widget

--- src/components/widget/ReviewScore.jsx
+++ src/components/widget/ReviewScore.jsx
@@ -2,8 +2,88 @@
 
 const ReviewScore = () => {
     return (
-        <div>
-            ReviewScore
+        <div className="widget-wrap">
+            <h3 className="widget-title">Review Score</h3>
+            <div className="widget-body">
+                <ul className="category-list">
+                    <li className="custom-check-box">
+                        <div className="custom-controls-stacked">
+                            <label className="custom-control material-checkbox">
+                                <input type="checkbox" className="material-control-input" />
+                                <span className="material-control-indicator"></span>
+                                <span className="description">
+                                    <i className="ri-star-fill star-fill"></i>
+                                    <i className="ri-star-fill star-fill"></i>
+                                    <i className="ri-star-fill star-fill"></i>
+                                    <i className="ri-star-fill star-fill"></i>
+                                    <i className="ri-star-fill star-fill"></i>
+                                </span>
+                            </label>
+                        </div>
+                    </li>
+
+                    <li className="custom-check-box">
+                        <div className="custom-controls-stacked">
+                            <label className="custom-control material-checkbox">
+                                <input type="checkbox" className="material-control-input" />
+                                <span className="material-control-indicator"></span>
+                                <span className="description">
+                                    <i className="ri-star-fill star-fill"></i>
+                                    <i className="ri-star-fill star-fill"></i>
+                                    <i className="ri-star-fill star-fill"></i>
+                                    <i className="ri-star-fill star-fill"></i>
+                                    <i className="ri-star-fill"></i>
+                                </span>
+                            </label>
+                        </div>
+                    </li>
+                    <li className="custom-check-box">
+                        <div className="custom-controls-stacked">
+                            <label className="custom-control material-checkbox">
+                                <input type="checkbox" className="material-control-input" />
+                                <span className="material-control-indicator"></span>
+                                <span className="description">
+                                    <i className="ri-star-fill star-fill"></i>
+                                    <i className="ri-star-fill star-fill"></i>
+                                    <i className="ri-star-fill star-fill"></i>
+                                    <i className="ri-star-fill"></i>
+                                    <i className="ri-star-fill"></i>
+                                </span>
+                            </label>
+                        </div>
+                    </li>
+                    <li className="custom-check-box">
+                        <div className="custom-controls-stacked">
+                            <label className="custom-control material-checkbox">
+                                <input type="checkbox" className="material-control-input" />
+                                <span className="material-control-indicator"></span>
+                                <span className="description">
+                                    <i className="ri-star-fill star-fill"></i>
+                                    <i className="ri-star-fill star-fill"></i>
+                                    <i className="ri-star-fill"></i>
+                                    <i className="ri-star-fill"></i>
+                                    <i className="ri-star-fill"></i>
+                                </span>
+                            </label>
+                        </div>
+                    </li>
+                    <li className="custom-check-box">
+                        <div className="custom-controls-stacked">
+                            <label className="custom-control material-checkbox">
+                                <input type="checkbox" className="material-control-input" />
+                                <span className="material-control-indicator"></span>
+                                <span className="description">
+                                    <i className="ri-star-fill star-fill"></i>
+                                    <i className="ri-star-fill"></i>
+                                    <i className="ri-star-fill"></i>
+                                    <i className="ri-star-fill"></i>
+                                    <i className="ri-star-fill"></i>
+                                </span>
+                            </label>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
