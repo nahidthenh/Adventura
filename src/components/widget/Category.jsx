7f626b48eae88d47e4e62aa0e52c71a0fbd7bda8
nahidthenh <nhgroup18@gmail.com>
2023-07-09 18:54:08 +0600

    Category widget

--- src/components/widget/Category.jsx
+++ src/components/widget/Category.jsx
@@ -2,8 +2,58 @@
 
 const Category = () => {
     return (
-        <div>
-            Category
+        <div className="widget-wrap">
+            <h3 className="widget-title">Category</h3>
+            <div className="widget-body">
+                <ul className="category-list">
+                    <li className="custom-check-box">
+                        <div className="custom-controls-stacked">
+                            <label className="custom-control material-checkbox">
+                                <input type="checkbox" className="material-control-input" />
+                                <span className="material-control-indicator"></span>
+                                <span className="description">Adventure Tours</span>
+                            </label>
+                        </div>
+                    </li>
+                    
+                    <li className="custom-check-box">
+                        <div className="custom-controls-stacked">
+                            <label className="custom-control material-checkbox">
+                                <input type="checkbox" className="material-control-input" />
+                                <span className="material-control-indicator"></span>
+                                <span className="description">City Tours</span>
+                            </label>
+                        </div>
+                    </li>
+                    <li className="custom-check-box">
+                        <div className="custom-controls-stacked">
+                            <label className="custom-control material-checkbox">
+                                <input type="checkbox" className="material-control-input" />
+                                <span className="material-control-indicator"></span>
+                                <span className="description">Couple Tours</span>
+                            </label>
+                        </div>
+                    </li>
+                    <li className="custom-check-box">
+                        <div className="custom-controls-stacked">
+                            <label className="custom-control material-checkbox">
+                                <input type="checkbox" className="material-control-input" />
+                                <span className="material-control-indicator"></span>
+                                <span className="description">Group Tours</span>
+                            </label>
+                        </div>
+                    </li>
+                    <li className="custom-check-box">
+                        <div className="custom-controls-stacked">
+                            <label className="custom-control material-checkbox">
+                                <input type="checkbox" className="material-control-input" />
+                                <span className="material-control-indicator"></span>
+                                <span className="description">Hosted Tours</span>
+                            </label>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
