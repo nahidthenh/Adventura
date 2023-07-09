a3d0d852af4f7905e36243f52fa4001e0cac39c4
nahidthenh <nhgroup18@gmail.com>
2023-07-09 18:56:39 +0600

    Durations Page

--- src/components/widget/Durations.jsx
+++ src/components/widget/Durations.jsx
@@ -2,8 +2,58 @@
 
 const Durations = () => {
     return (
-        <div>
-            Durations
+        <div className="widget-wrap">
+            <h3 className="widget-title">Durations</h3>
+            <div className="widget-body">
+                <ul className="category-list">
+                    <li className="custom-check-box">
+                        <div className="custom-controls-stacked">
+                            <label className="custom-control material-checkbox">
+                                <input type="checkbox" className="material-control-input" />
+                                <span className="material-control-indicator"></span>
+                                <span className="description">0 - 24 hours</span>
+                            </label>
+                        </div>
+                    </li>
+
+                    <li className="custom-check-box">
+                        <div className="custom-controls-stacked">
+                            <label className="custom-control material-checkbox">
+                                <input type="checkbox" className="material-control-input" />
+                                <span className="material-control-indicator"></span>
+                                <span className="description">1 - 2 days</span>
+                            </label>
+                        </div>
+                    </li>
+                    <li className="custom-check-box">
+                        <div className="custom-controls-stacked">
+                            <label className="custom-control material-checkbox">
+                                <input type="checkbox" className="material-control-input" />
+                                <span className="material-control-indicator"></span>
+                                <span className="description">2 - 3 days</span>
+                            </label>
+                        </div>
+                    </li>
+                    <li className="custom-check-box">
+                        <div className="custom-controls-stacked">
+                            <label className="custom-control material-checkbox">
+                                <input type="checkbox" className="material-control-input" />
+                                <span className="material-control-indicator"></span>
+                                <span className="description">3 - 4 days</span>
+                            </label>
+                        </div>
+                    </li>
+                    <li className="custom-check-box">
+                        <div className="custom-controls-stacked">
+                            <label className="custom-control material-checkbox">
+                                <input type="checkbox" className="material-control-input" />
+                                <span className="material-control-indicator"></span>
+                                <span className="description">4 - 5 days</span>
+                            </label>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
