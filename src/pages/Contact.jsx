78faeba6b1ea1bef08ba246e9ca14c444d353560
nahidthenh <nhgroup18@gmail.com>
2023-07-10 21:01:00 +0600

    404 Page Design

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,23 @@
 
-
+import { Col, Container, Row } from 'react-bootstrap';
+import Breadcrumbs from '../components/Breadcrumbs'
 const Contact = () => {
     return (
         <div className="default-margin-top">
-            <h1>Comming Soon...</h1>
+            <Breadcrumbs title='Contact Us' description='Fell Free To Contact With Us.'></Breadcrumbs>
+            <Container>
+                <Row>
+                    <Col lg={4} md={6} sm={12}>
+                        <div className="single-info-box">
+                            <div className="inner-box">
+                                <div className="icon-box"><i className="ri-location-line"></i></div>
+                                <h3>Address</h3>
+                                <p>31 park street, 5th Avenue, Dhanmondy, Dhaka.</p>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,22 @@
-
+import { Link } from 'react-router-dom';
+import { Header, Footer } from '../pages/index'
 
 const Error = () => {
     return (
-        <div>
-            404 Page Not Found , Error
-        </div>
+        <>
+            <Header></Header>
+            <div className="default-margin-top">
+                <div className="error-wrap">
+                    <h1>404</h1>
+                    <h2>
+                        page is not found. <br />
+                        the page is doesnt exist or deleted.
+                    </h2>
+                    <Link to='/'>GO TO HOME</Link>
+                </div>
+            </div>
+            <Footer></Footer>
+        </>
     );
 };
 
