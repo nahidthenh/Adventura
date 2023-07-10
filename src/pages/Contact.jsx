b13ee9c55ed163c1762b8df1e458b81381db9c2d
nahidthenh <nhgroup18@gmail.com>
2023-07-10 21:21:05 +0600

    Contact form top section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,14 +6,39 @@ const Contact = () => {
         <div className="default-margin-top">
             <Breadcrumbs title='Contact Us' description='Fell Free To Contact With Us.'></Breadcrumbs>
             <Container>
-                <Row>
+                <Row className='py-5'>
                     <Col lg={4} md={6} sm={12}>
-                        <div className="single-info-box">
-                            <div className="inner-box">
-                                <div className="icon-box"><i className="ri-location-line"></i></div>
-                                <h3>Address</h3>
-                                <p>31 park street, 5th Avenue, Dhanmondy, Dhaka.</p>
-                            </div>
+                        <div className="contact-box">
+                            <div className="icon-box"><i className="ri-location-line"></i></div>
+                            <h3>Address</h3>
+                            <p>Khilkhet House 12 , Nikunja 2 1250 Dhaka Bangladesh.</p>
+                        </div>
+                    </Col>
+                    <Col lg={4} md={6} sm={12}>
+                        <div className="contact-box">
+                            <div className="icon-box"><i className="ri-location-line"></i></div>
+                            <h3>Phone</h3>
+                            <ul>
+                                <li><a href="tel:987456321">+987456321</a></li>
+                                <li><a href="tel:125478965">+125478965</a></li>
+                            </ul>
+                        </div>
+                    </Col>
+                    <Col lg={4} md={6} sm={12}>
+                        <div className="contact-box">
+                            <div className="icon-box"><i className="ri-location-line"></i></div>
+                            <h3>Email</h3>
+                            <ul>
+                                <li><a href="mailto:youremail@gmail.com">youremail@gmail.com</a></li>
+                                <li><a href="mailto:youremail@gmail.com">youremail@gmail.com</a></li>
+                            </ul>
+                        </div>
+                    </Col>
+                </Row>
+                <Row className='pb-5'>
+                    <Col lg={6} className='mx-auto'>
+                        <div className="contact-form-wrap p-3">
+
                         </div>
                     </Col>
                 </Row>
