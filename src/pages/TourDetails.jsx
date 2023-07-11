0c6a71e4b0469ca6023570e02d8c2c68878807c1
nahidthenh <nhgroup18@gmail.com>
2023-07-11 20:58:15 +0600

    Tour Details Page Banner

--- src/pages/TourDetails.jsx
+++ src/pages/TourDetails.jsx
@@ -1,12 +1,23 @@
+import { Col, Container, Row } from "react-bootstrap";
 import { useLoaderData } from "react-router-dom";
 
 const TourDetails = () => {
     const tours = useLoaderData()
-    console.log(tours);
+    const { rating, place, price } = tours
     return (
         <div className="default-margin-top">
-            <div className="tours-details-baner py-5 bg-danger">
-                <h3>Hello</h3>
+            <div className="tours-details-baner">
+                <Container>
+                    <Row>
+                        <Col lg={8}>
+                            <div className="tours-banner-content">
+                                <div className="rating"><span><i className="ri-star-fill"></i>{rating} Superb</span></div>
+                                <h1>{place}</h1>
+                                <h3>{price}<span> / Per person</span></h3>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
             <h3>{tours.place}</h3>
         </div>
