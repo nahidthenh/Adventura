e1591e92cf09c2cd2352e265c013fc020b698179
nahidthenh <nhgroup18@gmail.com>
2023-07-11 22:06:04 +0600

    Book Form Widget Done

--- src/components/widget/BookForm.jsx
+++ src/components/widget/BookForm.jsx
@@ -4,17 +4,15 @@ const BookForm = () => {
         <div className="widget-wrap">
             <h3 className="widget-title">Book This Tour.</h3>
             <div className="widget-body">
-                <ul className="category-list">
-                    <li className="custom-check-box">
-                        <div className="custom-controls-stacked">
-                            <label className="custom-control material-checkbox">
-                                <input type="checkbox" className="material-control-input" />
-                                <span className="material-control-indicator"></span>
-                                <span className="description">Adventure Tours</span>
-                            </label>
-                        </div>
-                    </li>
-                </ul>
+                <form className="book-form">
+                    <input className="auth-input mb-4" type="text" name="name" placeholder="Your Name" required />
+                    <input className="auth-input mb-4" type="email" name="email" placeholder="Your Email" required />
+                    <input className="auth-input mb-4" type="phone" name="phone" placeholder="Phone" required />
+                    <input className="auth-input mb-4" type="text" name="number" placeholder="Tckets" required />
+                    <input className="auth-input mb-4" type="date" name="date" placeholder="Date" required />
+                    <textarea name="message" className="auth-input mb-4" placeholder="Message" id="" cols="30" rows="10" required></textarea>
+                    <button disabled type="submit">Book Now</button>
+                </form>
             </div>
         </div>
     );
