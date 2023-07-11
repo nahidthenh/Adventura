35d0ee017ec7fc65778cf8d9fe33b6a844ab96ae
nahidthenh <nhgroup18@gmail.com>
2023-07-11 22:38:53 +0600

    Blog Details

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,7 +1,33 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { useLoaderData } from "react-router-dom";
+import Breadcrumbs from "../components/Breadcrumbs";
+import { Category, Download, Search, Sponsor } from "../components/widget";
 const BlogDetails = () => {
+    const blogs = useLoaderData()
+    const { image, blogTitle, description, postDate } = blogs
     return (
         <div className="default-margin-top">
-            <h3>Welcome Blog Details Page</h3>
+            <Breadcrumbs title="Our Travel Blog" description="Welcome to our travel Blog"></Breadcrumbs>
+            <div className="blog-details-wrap py-5">
+                <Container>
+                    <Row>
+                        <Col lg={8} md={12} sm={12}>
+                            <img src={image} alt="" />
+                            <small>{postDate}</small>
+                            <h3>{blogTitle}</h3>
+                            <p>{description}</p>
+                        </Col>
+                        <Col lg={4} md={12} sm={12}>
+                            <div className="ms-3 sidebar-sticky">
+                                <Search></Search>
+                                <Download></Download>
+                                <Category></Category>
+                                <Sponsor></Sponsor>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 };
