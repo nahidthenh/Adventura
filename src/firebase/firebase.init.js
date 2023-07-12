a1a6ee748f3724321b3c4f609c41a323d1db6335
nahidthenh <nhgroup18@gmail.com>
2023-07-12 07:59:08 +0600

    FirebaseInit

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDaOVLyz7O2_TvuOSG182QqnSzgyIQoD-k",
+    authDomain: "adventura5.firebaseapp.com",
+    projectId: "adventura5",
+    storageBucket: "adventura5.appspot.com",
+    messagingSenderId: "316939141002",
+    appId: "1:316939141002:web:aeccd0145f29fd30a4d273"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
