ab47299c7a2a1d96d60c1c41710c2fb290493d37
nahidthenh <nhgroup18@gmail.com>
2023-07-12 16:57:32 +0600

    Profile Components Update

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,20 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthProvider";
+import { Col, Container, Row } from "react-bootstrap";
+
 const Profile = () => {
+    const { user } = useContext(AuthContext)
     return (
-        <div>
-            <h3>Welcome, </h3>
-            <p>Profile Page</p>
+        <div className="default-margin-top">
+            <Container className="py-5">
+                <Row>
+                    <Col lg={12}>
+                        <h1>Welcome, {user.displayName}</h1>
+                        <p>Profile Page</p>
+                        <small className="text-danger">This Page Design underConstraction </small>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
