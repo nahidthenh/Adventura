7a3fe23ce57511306ebbf83b6999a58755784cfe
nahidthenh <nhgroup18@gmail.com>
2023-07-12 08:26:06 +0600

    SignIn With Google

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,13 +1,25 @@
 /* eslint-disable react/prop-types */
 import { createContext } from 'react';
-
+import app from './../firebase/firebase.init';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
 export const AuthContext = createContext()
 
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
 const AuthProvider = ({ children }) => {
 
+    // Google SignIn
 
-    const authInfo = { displayName: 'Nahid' }
+    // Google login 
+    const googleLogin = () => {
+        // setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    // Send Value 
+    const authInfo = { googleLogin }
 
     return (
         <AuthContext.Provider value={authInfo}>
