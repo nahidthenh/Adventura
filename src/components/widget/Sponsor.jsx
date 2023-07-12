4f1982681d779e17e9830bb2ce9d5b11ee87885c
nahidthenh <nhgroup18@gmail.com>
2023-07-12 07:45:19 +0600

    Sponsor

--- src/components/widget/Sponsor.jsx
+++ src/components/widget/Sponsor.jsx
@@ -1,11 +1,11 @@
-import adds from '../../assets/images/addds.png'
+
 
 const Sponsor = () => {
     return (
         <div className="widget-wrap">
             <h3 className="widget-title">Get Special Discount</h3>
             <div className="widget-body">
-                <img src={adds} alt="ads" className='img-fluid rounded-4' />
+                <img src="https://picsum.photos/1080/1920?random=1" alt="ads" className='img-fluid rounded-4' />
             </div>
         </div>
     );
