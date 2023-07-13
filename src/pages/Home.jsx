70992a2d5e528f3430503b498527ce0c84011164
nahidthenh <nhgroup18@gmail.com>
2023-07-13 12:23:04 +0600

    Tablete Responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -231,8 +231,8 @@ const Home = () => {
                                             spaceBetween: 20,
                                         },
                                         768: {
-                                            slidesPerView: 4,
-                                            spaceBetween: 40,
+                                            slidesPerView: 2,
+                                            spaceBetween: 20,
                                         },
                                         1024: {
                                             slidesPerView: 5,
